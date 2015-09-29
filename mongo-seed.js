630b1ee87e0f370ad557034bba0fcaf03105428f
Tejas Deshpande <tejasd@outlook.com>
2015-09-29 01:02:43 -0400

    Added mongo seed script

---
+++ mongo-seed.js
@@ -0,0 +1,17 @@
+db.katta.insert({
+ _id: 1, 
+ name:"Tejas Deshpande", 
+ friendsWhoWantToHangWithMe: [2, 3],
+});
+
+db.katta.insert({
+ _id: 2, 
+ name:"Ayushi Behede", 
+ friendsWhoWantToHangWithMe: [1],
+});
+
+db.katta.insert({
+ _id: 3, 
+ name:"Evan Sale", 
+ friendsWhoWantToHangWithMe: [1],
+});
